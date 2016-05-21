fe8eec44bb6a4e82275841d48e6372d95d274626
FunATuns <payton.obrycki@gmail.com>
2016-05-21 17:39:24 -0500

    Initial commit

---
+++ Isleward - Coordinates.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Isleward - Coordniates 
+// @namespace    Isleward.Addon
+// @version      0.1
+// @description  Displays the current coordinates of the user
+// @author       Anubis5
+// @match        isleward-test.herokuapp.com/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    var scriptElement = document.createElement( "script" );
+    scriptElement.type = "text/javascript";
+    scriptElement.src = "https://cdn.rawgit.com/Isleward-Addon-Dev-Group/PlayerCoordinates/master/coordinates.js";
+    document.body.appendChild( scriptElement );
+})();
