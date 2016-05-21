697cf8e475b502c75656aadcb9740c576f7e79a9
FunATuns <payton.obrycki@gmail.com>
2016-05-21 18:06:18 -0500

    YEAH ITS ALL GOOD NOW :DDDD

--- map.js
+++ map.js
@@ -6,16 +6,24 @@ addons.register({
         
         this.uiCoordinates.css({
             'position': "absolute",
-            'left': (this.uiContainer[0].clientWidth / 3),
-            'top':  (this.uiContainer[0].clientWidth / 3) ,
+            'left': (this.uiContainer[0].clientWidth / 4.5),
+            'top':  (this.uiContainer[0].clientHeight / 32) ,
             'background-color': "#3c3f4c",
-            'border': "4px solid #505360",
+            'border': "4px solid #3c3f4c",
+            'color':"white",
         });
 		
-        //this.uiCoordinates.css("display", "none");
-        //events.on('onGetMap', this.onGetMap.bind(this));
+        this.uiCoordinates.css("display", "none");
+        events.on('onGetMap', this.onGetMap.bind(this));
         //events.on('onKeyDown', this.onKeyDown.bind(this));
         events.on('onGetObject', this.onGetObject.bind(this));
+    },
+	onGetMap: function(mapData) {
+        if (!mapData.collisionMap) {
+            return;   
+        }
+            
+        this.uiCoordinates.css("display", "block");
     },
     onGetObject: function(object) {
         if (!object.id) {
